978575d7c6d2b6b9aeb2134a6d54293822f0fe52
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-12-12 17:30:20 -0700

    initial commit. basic setup completed

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import path from 'path'
+import express from 'express'
+import { MongoClient } from 'mongodb'
+import template from './../template'
+// comment out before building for prod
+import devBundle from './devBundle'
+
+const app = express()
+// comment out before building for prod
+devBundle.compile(app)
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.get('/', (req, res) => {
+  res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', port)
+})
+
+// DB connection URL
+const url =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+MongoClient.connect(url, (err, db) => {
+  console.log('Connected successfully to mongodb server')
+  db.close()
+})
