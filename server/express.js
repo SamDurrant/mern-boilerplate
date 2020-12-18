ff893fd4dc371c6e7573b9ab486c61c40c51f74a
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-12-17 19:32:21 -0700

    finish backend. add auth and controllers for routes.

--- server/express.js
+++ server/express.js
@@ -6,6 +6,9 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from './../template'
+// routes
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
 // comment out before building for prod
 import devBundle from './devBundle'
@@ -25,8 +28,22 @@ app.use(cors())
 
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
+// mount routes
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
 app.get('/', (req, res) => {
   res.status(200).send(Template())
+})
+
+// catch unauthorized errors
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: err.name + ': ' + err.message })
+  } else if (err) {
+    res.status(400).json({ error: err.name + ': ' + err.message })
+    console.log(err)
+  }
 })
 
 export default app
